1428d3e276b8e5a5e60d322e923ab0c15aed0b99
dmitry-kornienko <dima.korn1996@gmail.com>
2023-10-07 12:42:58 +0400

    added backend path

--- src/app/services/api.ts
+++ src/app/services/api.ts
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery, retry } from '@reduxjs/toolkit/query/react';
 import { RootState } from '../store'; 
 
 const baseQuerry = fetchBaseQuery({
-    baseUrl: 'http://localhost:8000/api',
+    baseUrl: 'https://wb-vgq7.onrender.com/api',
     prepareHeaders(headers, { getState }) {
         const token = (getState() as RootState).auth.user?.token ||
             localStorage.getItem('token');
